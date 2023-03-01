c5f1214b07d52b87c92851e32887b0246204e977
MaxChuloshnikov <Maks447@ukr.net>
2023-03-01 11:50:00 +0200

    Project Done

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -40,12 +40,36 @@ const CreatePost = () => {
     }
   }
 
-  const handleSubmit = () => {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
+    if(form.prompt && form.photo) {
+      setLoading(true);
+
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(form)
+        })
+
+        await response.json();
+        navigate('/');
+      } catch (error) {
+        alert(error);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert('Please enter a prompt and generate an image');
+    }
   }
 
   const handleChange = (event) => {
-    setForm({ ...form, [e.target.name]: e.target.value })
+    setForm({ ...form, [event.target.name]: event.target.value })
   }
 
   const handleSurpriseMe = () => {
